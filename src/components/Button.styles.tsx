77b7c61fd182cf52b9c100f1d95192c999aac633
Eduardo Da Veiga Spiandorello <eduardo@arcotech.io>
2022-10-16 20:39:05 -0300

    Initial commit

---
+++ src/components/Button.styles.tsx
@@ -0,0 +1,21 @@
+import styled, {css} from 'styled-components';
+
+export type ButtonVariants = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonProps {
+    variant: ButtonVariants,
+}
+
+const buttonVariants = {
+    'primary': 'blue',
+    'secondary': 'purple',
+    'success': 'green',
+    'danger': 'red',
+};
+
+export const ButtonContainer = styled.button<ButtonProps>`
+  ${props => css`background-color: ${props.theme['green-500']}`};
+
+  height: 40px;
+  width: 200px;
+`;
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonContainer, ButtonVariants } from './Button.styles';
+
+interface ButtonProps {
+    variant?: ButtonVariants;
+}
+
+export function Button(props: ButtonProps) {
+    const { variant = 'primary' } = props;
+
+    return (
+      <ButtonContainer variant={variant}>
+          Click me
+      </ButtonContainer>
+    );
+}
